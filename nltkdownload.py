af40bcd338382d7bd852d75948a62ccfd398665f
rvaidun <19789246+rvaidun@users.noreply.github.com>
2024-10-14 12:35:17 -0700

    Revert "Update nltk modules"

--- nltkdownload.py
+++ nltkdownload.py
@@ -10,6 +10,6 @@ else:
 
 
 nltk.download('wordnet')
-nltk.download('averaged_perceptron_tagger_eng')
-nltk.download('punkt_tab')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('punkt')
 
